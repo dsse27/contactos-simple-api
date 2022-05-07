220d4b31ddb4a3983a9bea50180048cdbf2f3885
Daniel Salazar <daniel.salazar@netmagica.com>
2022-05-07 17:08:02 -0400

    Implementar mongoose de nuevo

--- config/mongo.js
+++ config/mongo.js
@@ -3,8 +3,27 @@ const {
     ServerApiVersion
 } = require('mongodb');
 
+const mongoose = require('mongoose')
+
 const dbConnect = () => {
     const DB_URI = process.env.DB_URI
+    
+    mongoose.connect(DB_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        connectTimeoutMS: 500
+    }, (err, res) => {
+        if (!err) {
+            console.log('**** CONEXION CORRECTA ****')
+        } else {
+            console.log('***** ERROR DE CONEXION ****')
+        }
+    })
+}
+
+const dbConnectVersionDirecta = () => {
+    const DB_URI = process.env.DB_URI
+
     const client = new MongoClient(DB_URI, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
